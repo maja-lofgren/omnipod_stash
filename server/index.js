569b909a85c628cca05d02661aa05cbc1db8fb79
Karl Hagström <karl.hagstrom@lm.se>
2022-01-02 21:58:52 +0100

    api + site

--- server/index.js
+++ server/index.js
@@ -1,12 +1,45 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const express = require('express');
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello Worlds</h1>');
+const isDev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 5000;
+
+
+const app = express();
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../build')));
+
+// Answer API requests.
+app.get('/api', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send('{"message":"Hello from the custom server!"}');
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
+
+app.listen(PORT, function () {
+  console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+});
+
+
+
+
+
+
+
+// const http = require('http');
+// const port = process.env.PORT || 3000
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello Worlds</h1>');
+// });
+
+// server.listen(port,() => {
+//   console.log(`Server running at port `+port);
+// });
