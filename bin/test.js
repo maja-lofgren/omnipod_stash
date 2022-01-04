a079c6296d4a8f061faf54d9f1986dc4f6914716
Karl Hagström <karl.hagstrom@lm.se>
2022-01-04 23:26:32 +0100

    works!

--- bin/test.js
+++ bin/test.js
@@ -1,7 +1,3 @@
-function getLastKnownPodChange() {
-
-}
-
 function checkOmnipodState() {
     // we create 'users' collection in newdb database
     var url = "mongodb://localhost:27017/nightscout";
@@ -19,26 +15,22 @@ function checkOmnipodState() {
         db.createCollection("omnipodstash", function (err, result) {
             if (err) {
                 if (err.message == 'Collection already exists. NS: nightscout.omnipodstash') {
-                    console.log("collection already exists!")
+                    console.log("omnipodstash exists!")
                 }
                 else {
                     throw err;
                 }
             }
             else {
-                console.log("Collection is created!");
+                console.log('Collection "omnipodstash" is created!');
             }
-            /** TOdo
-             * kör en find likt nedan för att se om det har använts fler poddar sen sist
-             * spara undan datum för sista podden som slängdes (alt id?) för att inte leta längre än till den
-             * 
-            */
-            //var query = { address: "Park Lane 38" };
+
+            //fetch last entry in omnipodstash
             db.collection("omnipodstash")
                 .find({}, { projection: { _id: 0 } })
                 .sort({ $natural: -1 }) //last
                 .limit(1)
-                .next() //hittar första...
+                .next() //open first return...
                 .then(
                     function (doc) {
                         var count = 0;
@@ -46,11 +38,12 @@ function checkOmnipodState() {
                         if (doc != null && doc.hasOwnProperty('OmnipodCount')) {
                             count = doc.OmnipodCount;
                             lastKnownPodChange = doc.LastKnownPodChange;
+                            console.log('count: ' + count);
+                            console.log('lastKnownPodChange: ' + lastKnownPodChange);
                         }
-                        console.log('count: ' + count);
-                        console.log('lastKnownPodChange: ' + lastKnownPodChange);
+
                         if (lastKnownPodChange != null) {
-                            //find nr of new pod-changes: 
+                            //fetch nr of new pod-changes: 
                             db.collection("treatments")
                                 .aggregate([
                                     {
@@ -70,22 +63,41 @@ function checkOmnipodState() {
                                 ])
                                 .next()
                                 .then(function (doc) {
-                                    console.log(doc.count);
-                                    let newUsedPods = doc.count;
-                                    
-
-                                    var myobj = {
-                                        date: new Date().toISOString(),
-                                        diff: -newUsedPods,
-                                        OmnipodCount: count-newUsedPods,
-                                        LastKnownPodChange: "2021-10-11T16:18:08Z"
-                                    };
-                                    db.collection("omnipodstash").insertOne(myobj, function (err, res) {
-                                        if (err) throw err;
-                                        console.log("1 document inserted");
-                                        // close the connection to db when you are done with it
+                                    if (doc == null) {
+                                        console.log("no new pod found");
                                         dbClient.close();
-                                    });
+                                        return;
+                                    }
+                                    let newUsedPods = doc.count;
+                                    console.log("newUsedPods:" + newUsedPods);
+
+                                    //fetch date of last used pod: 
+                                    db.collection("treatments")
+                                        .find({}, { projection: { created_at: 1 } })
+                                        .sort({ $natural: -1 })
+                                        .limit(1)
+                                        .next()
+                                        .then(
+                                            function (doc) {
+                                                let lastPodChange = doc.created_at;
+                                                //create new db-object
+                                                var dbEntity = {
+                                                    date: new Date().toISOString(),
+                                                    diff: -newUsedPods,
+                                                    OmnipodCount: count - newUsedPods,
+                                                    LastKnownPodChange: lastPodChange//"2021-10-11T16:18:08Z"
+                                                };
+                                                //update omnipodstash with latest: 
+                                                db.collection("omnipodstash").insertOne(dbEntity, function (err, res) {
+                                                    if (err) throw err;
+                                                    console.log("1 document inserted:");
+                                                    console.log(dbEntity);
+                                                    // close the connection to db when you are done with it
+                                                    dbClient.close();
+                                                });
+                                            });
+
+
                                 },
                                     function (err) {
                                         console.log('Error:', err);
@@ -93,20 +105,33 @@ function checkOmnipodState() {
                                 );
 
                         } else {
-                            //todo gör ingenting!
-                            var nya = 5;
-                            var myobj = {
-                                date: new Date().toISOString(),
-                                diff: nya,
-                                OmnipodCount: nya + count,
-                                LastKnownPodChange: "2021-10-11T16:18:08Z"
-                            };
-                            db.collection("omnipodstash").insertOne(myobj, function (err, res) {
-                                if (err) throw err;
-                                console.log("1 document inserted");
-                                // close the connection to db when you are done with it
-                                dbClient.close();
-                            });
+                            //first time = omnipodstash is empty...
+                            //update with latest pod and zero stash:
+                            //fetch date of last used pod: 
+                            db.collection("treatments")
+                                .find({}, { projection: { created_at: 1 } })
+                                .sort({ $natural: -1 })
+                                .limit(1)
+                                .next()
+                                .then(
+                                    function (doc) {
+                                        let lastPodChange = doc.created_at;
+                                        var dbEntity = {
+                                            date: new Date().toISOString(),
+                                            diff: 0,
+                                            OmnipodCount: 0,
+                                            LastKnownPodChange: lastPodChange
+                                        };
+                                        //update db: 
+                                        db.collection("omnipodstash").insertOne(dbEntity, function (err, res) {
+                                            if (err) throw err;
+                                            console.log("1 document inserted:");
+                                            console.log(dbEntity);
+                                            // close the connection to db when you are done with it
+                                            dbClient.close();
+                                        });
+                                    });
+
                         }
 
                     },
@@ -115,37 +140,6 @@ function checkOmnipodState() {
                     }
                 );
 
-            //insert
-            // db.collection("omnipodstash")
-            //     .find({}, { projection: { _id: 0 } })
-            //     .sort({ $natural: -1 })
-            //     .limit(1)
-            //     .next()
-            //     .then(
-            //         function (doc) {
-            //             var count = 0;
-            //             if (doc != null && doc.hasOwnProperty('OmnipodCount')) count = doc.OmnipodCount;
-            //             console.log(count);
-            //             var nya = 5;
-            //             var myobj = {
-            //                 date: new Date().toISOString(),
-            //                 diff: nya,
-            //                 OmnipodCount: nya + count,
-            //                 LastChangedPod: "2021-12-12T20:23:28.802Z"
-            //             };
-            //             db.collection("omnipodstash").insertOne(myobj, function (err, res) {
-            //                 if (err) throw err;
-            //                 console.log("1 document inserted");
-            //                 // close the connection to db when you are done with it
-            //                 dbClient.close();
-            //             });
-            //         },
-            //         function (err) {
-            //             console.log('Error:', err);
-            //         }
-            //     );
-
-
 
         });
 
@@ -153,46 +147,5 @@ function checkOmnipodState() {
 
     });
 
-    // var MongoClient = require('mongodb').MongoClient, format = require('util').format;
-
-    // MongoClient.connect('mongodb://127.0.0.1:27017/nightscout', function(err, a) {
-    //     const db = a.db('nightscout');
-    //     if(err) throw err;   
-    //     db.collectionNames(function(err, collections){
-    //         console.log(collections);   
-    //     }); 
-    // });
 }
 checkOmnipodState()
-
-
-
-//// db.collection("treatments")
-                                    //     .find({
-                                    //         "created_at": {
-                                    //             "$gt": lastKnownPodChange
-                                    //         },
-                                    //         "eventType": "Insulin Change"
-                                    //     },
-                                    //         {
-                                    //             projection: {
-                                    //                 "created_at": 1,
-                                    //                 "eventType": 1
-                                    //             }
-                                    //         })
-                                    //     .sort({ "created_at":-1 })
-                                    //     .toArray(function (err, doc) {
-                                    // var count = 0;
-                                    // if (doc != null && doc.hasOwnProperty('OmnipodCount')) count = doc.OmnipodCount;
-                                    // console.log(count);
-                                    // var nya = 5;
-                                    // var myobj = {
-                                    //     date: new Date().toISOString(),
-                                    //     diff: nya,
-                                    //     OmnipodCount: nya + count,
-                                    //     LastChangedPod: "2021-12-12T20:23:28.802Z"
-                                    // };
-                                    // db.collection("omnipodstash").insertOne(myobj, function (err, res) {
-                                    //     if (err) throw err;
-                                    //     console.log("1 document inserted");
-                                    // });
