a9e6deedeceed6a4fb814ee9abe48d75c2b33776
Karl Hagström <karl.hagstrom@lm.se>
2022-01-10 10:06:20 +0100

    buggfix

--- server/updatedb.js
+++ server/updatedb.js
@@ -96,7 +96,7 @@ async function getPodCount() {
         return;
     }
     var count = 0;
-    var lastKnownPodChange = null;
+    var lastKnownPodChange = new Date().toISOString();
     try {
         //fetch last entry in omnipodstash
         let doc = await db.collection("omnipodstash")
