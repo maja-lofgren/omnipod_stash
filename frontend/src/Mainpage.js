e5503d39ccfcf128069615282fb49601fda67dfe
Karl Hagström <karl.hagstrom@lm.se>
2022-01-16 01:47:27 +0100

    added source

--- frontend/src/Mainpage.js
+++ frontend/src/Mainpage.js
@@ -171,7 +171,7 @@ export default function Mainpage({ Typ }) {
             <br />
             <br />
             <div className="logg">
-                {lastActions?.map((it) => <text >{it.date.substring(0, 19).replace("T", " ") + " (UTC)"}, count: {it.Count}, diff: {it.diff} <br /> </text>)}
+                {lastActions?.map((it) => <text >{it.date.substring(0, 19).replace("T", " ") + " (UTC)"}, count: {it.Count}, diff: {it.diff}, source: {it.Source}<br /> </text>)}
                 {/* &#40;&#41; */}
             </div>
             <br />
